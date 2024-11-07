d736580c2ff5be7468925b803528c33095285e39
Alexander Myasnikov <a.myasnikov@stv.dunice.net>
2024-11-07 10:33:24 +0300

    Extract the spring initializr project in to the repo

---
+++ src/test/java/net/dunice/newsapi/NewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dunice.newsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
