f001487360177727f2a7a7b62303c0c806c4652b
Alexander Myasnikov <a.myasnikov@stv.dunice.net>
2024-11-20 11:43:06 +0300

    Remove redundant annotations

--- src/main/java/net/dunice/newsapi/entities/TagEntity.java
+++ src/main/java/net/dunice/newsapi/entities/TagEntity.java
@@ -1,14 +1,10 @@
 package net.dunice.newsapi.entities;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.ForeignKey;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -32,13 +28,6 @@ public class TagEntity {
     @Column(unique = true)
     String title;
 
-    @ManyToMany(cascade = CascadeType.PERSIST)
-    @JoinTable(
-            name = "news_with_tags",
-            joinColumns = @JoinColumn(name = "tag_id"),
-            foreignKey = @ForeignKey(name = "nwt_tags_fk"),
-            inverseJoinColumns = @JoinColumn(name = "news_id"),
-            inverseForeignKey = @ForeignKey(name = "nwt_news_fk")
-    )
+    @ManyToMany
     List<NewsEntity> news;
 }
